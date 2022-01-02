1ae8ec18645bf11d909031af675843b3259017e0
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-01-03 01:59:31 +0500

    hero ready

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com/'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
