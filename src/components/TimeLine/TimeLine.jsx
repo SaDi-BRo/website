f951711d19b09c8f6491e21d9f9f33a7f5781de9
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-07-24 08:24:05 +0500

    edited about me

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -66,11 +66,7 @@ const Timeline = () => {
   return (
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
-      <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established
-        developers to take their development skills to the next level and build
-        awesome apps.
-      </SectionText>
+      <SectionText>Information of me here</SectionText>
       <CarouselContainer ref={carouselRef} onScrol={handleScroll}>
         <>
           {TimeLineData.map((item, index) => (
