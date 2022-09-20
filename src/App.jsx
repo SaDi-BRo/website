104a22e835a5aac75b4f722005ba8a3627934f49
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-09-20 23:27:50 +0500

    ready about

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
