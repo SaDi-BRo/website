7718f1ec7471ae8af36fcc1908c980039e6b5a16
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-09-22 00:04:18 +0500

    ready skills

---
+++ src/components/Skills.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <div name="skills" className=" w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Skills
+          </p>
+          <p className="py-4">// These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src="/img/html.png" alt="HTML icon" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src="/img/css.png" alt="CSS icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="/img/javascript.png"
+              alt="JavaScript icon"
+            />
+            <p className="my-4">JavaScript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="/img/react.png"
+              alt="React JS icon"
+            />
+            <p className="my-4">React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="/img/github.png"
+              alt="GitHub icon"
+            />
+            <p className="my-4">GitHub</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="/img/node.png"
+              alt="Node JS icon"
+            />
+            <p className="my-4">Node JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="/img/mongo.png"
+              alt="MongoDB icon"
+            />
+            <p className="my-4">MongoDB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src="/img/aws.png" alt="AWS icon" />
+            <p className="my-4">AWS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
