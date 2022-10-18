31289a2fe785362ace7d65876ffc8173e754ff89
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-10-18 15:41:59 +0500

    updated work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,7 +13,7 @@ function Work() {
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
-            style={{ backgroundImage: 'url(/img/work.jpeg)' }}
+            style={{ backgroundImage: 'url(/img/beaches.png)' }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -21,12 +21,12 @@ function Work() {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
+                <a href="https://beaches-delta.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="#">
+                <a href="https://github.com/SaDi-BRo/beaches">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Source
                   </button>
@@ -35,7 +35,7 @@ function Work() {
             </div>
           </div>
           <div
-            style={{ backgroundImage: 'url(/img/work.jpeg)' }}
+            style={{ backgroundImage: 'url(/img/best-eats.png)' }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -43,12 +43,12 @@ function Work() {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
+                <a href="https://best-eats-mu.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="#">
+                <a href="https://github.com/SaDi-BRo/best-eats">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Source
                   </button>
@@ -57,7 +57,7 @@ function Work() {
             </div>
           </div>
           <div
-            style={{ backgroundImage: 'url(/img/work.jpeg)' }}
+            style={{ backgroundImage: 'url(/img/cloud-management.png)' }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -65,12 +65,12 @@ function Work() {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
+                <a href="https://cloud-management-eight.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="#">
+                <a href="https://github.com/SaDi-BRo/cloud-management">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Source
                   </button>
@@ -79,7 +79,7 @@ function Work() {
             </div>
           </div>
           <div
-            style={{ backgroundImage: 'url(/img/work.jpeg)' }}
+            style={{ backgroundImage: 'url(/img/data-finance.png)' }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -87,12 +87,12 @@ function Work() {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
+                <a href="https://data-finance-one.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="#">
+                <a href="https://github.com/SaDi-BRo/data-finance">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Source
                   </button>
@@ -101,7 +101,7 @@ function Work() {
             </div>
           </div>
           <div
-            style={{ backgroundImage: 'url(/img/work.jpeg)' }}
+            style={{ backgroundImage: 'url(/img/image-gallery.png)' }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -109,12 +109,12 @@ function Work() {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
+                <a href="https://image-gallery-kohl.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="#">
+                <a href="https://github.com/SaDi-BRo/image-gallery">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Source
                   </button>
@@ -123,7 +123,7 @@ function Work() {
             </div>
           </div>
           <div
-            style={{ backgroundImage: 'url(/img/work.jpeg)' }}
+            style={{ backgroundImage: 'url(/img/weather-app.png)' }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
@@ -131,12 +131,12 @@ function Work() {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="#">
+                <a href="https://weather-app-sadi.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="#">
+                <a href="https://github.com/SaDi-BRo/weather-app">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Source
                   </button>
