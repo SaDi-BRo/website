6535ed79dace9d8523138d594d72a5886eebf11f
SaDi <sayidullohabbasov0909@gmail.com>
2023-03-12 09:57:51 +0500

    removed date-fns

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import { compareDesc, format, parseISO } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
 
 export const metadata: Metadata = {
