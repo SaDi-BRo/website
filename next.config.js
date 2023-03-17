fd621f1e9f08da1de642b88b1e99bb40fc7bac3d
SaDi <sayidullohabbasov0909@gmail.com>
2023-03-17 22:03:22 +0500

    added spotify

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+    ],
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
