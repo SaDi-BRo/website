602f46db48c1b8b4b776f9553a80f95c125377f5
SaDi <sayidullohabbasov0909@gmail.com>
2023-03-19 10:49:29 +0500

    edited

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from 'next';
 
+function Link({ text, link }: { text: string; link: string }) {
+  return (
+    <a
+      className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 text-neutral-500 underline"
+      href={link}
+    >
+      {text}
+    </a>
+  );
+}
+
 export const metadata: Metadata = {
   title: 'Uses',
   description:
@@ -14,70 +25,94 @@ export default function Uses() {
         Here's what tech I'm currently using for coding, videos, and music.
       </p>
       <div className="prose prose-neutral dark:prose-invert">
-        <h3 className="mt-4 mb-1" id="computer-office">
-          Computer / Office
-        </h3>
-        <ul className="ml-5 list-disc text-neutral-500">
-          <li>16&quot; Macbook Pro (2020)</li>
-          <li>31.5&quot; LG UltraFine 32UN880</li>
-          <li>Logitech MX Master 3 Mouse</li>
-          <li>Apple Magic Keyboard</li>
-          <li>Focal XS 2.1 w/ Subwoofer</li>
-          <li>Autonomous SmartDesk 2</li>
-          <li>Autonomous ErgoChair 2</li>
-        </ul>
         <h3 className="mt-4 mb-1" id="coding">
           Coding
         </h3>
         <ul className="ml-5 list-disc text-neutral-500">
           <li>
-            Editor: VSCode (
-            <a
-              className="text-white underline"
-              href="https://gist.github.com/leerob/e7883ab35d900b8cbb684ac77e7c4703"
-            >
-              Settings / Extensions
-            </a>
-            )
+            My editor of choice is&nbsp;
+            <Link
+              text="Visual Studio Code"
+              link="https://code.visualstudio.com/"
+            />
+            &nbsp;with a giant pile of&nbsp;
+            <Link text="extensions" link="https://howivscode.com/SaDi-BRo" />.
           </li>
-          <li>Theme: One Dark Pro</li>
-          <li>Terminal: Hyper / zsh</li>
-        </ul>
-        <h3 className="mt-4 mb-1" id="audio-video">
-          Audio / Video
-        </h3>
-        <ul className="ml-5 list-disc text-neutral-500">
-          <li>Sony A7III (28-50mm, 35mm f1.8)</li>
-          <li>Shure SM7B + Elgato Wave XLR + Boom</li>
-          <li>Elgato Camlink</li>
-          <li>Logitech C920</li>
-          <li>Elgato Key Light (x2)</li>
-          <li>Elgato Master Mount</li>
-          <li>Aputure Mark 2 120D II</li>
-          <li>Aputure Light Dome II</li>
+          <li>
+            I am currently using&nbsp;
+            <Link
+              text="One Dark Pro"
+              link="https://marketplace.visualstudio.com/items?itemName=zhuangtongfa.Material-theme"
+            />
+            &nbsp;as my theme.
+          </li>
+          <li>
+            <Link
+              text="Material"
+              link="https://marketplace.visualstudio.com/items?itemName=PKief.material-icon-theme"
+            />
+            &nbsp;is currently my icon theme of choice.
+          </li>
         </ul>
         <h3 className="mt-4 mb-1" id="software">
-          Software
+          Terminal
         </h3>
         <ul className="ml-5 list-disc text-neutral-500">
-          <li>1Password</li>
-          <li>Spotify</li>
-          <li>CleanShot X</li>
-          <li>RetroClip</li>
-          <li>Grammarly</li>
-          <li>Texts</li>
-          <li>Raycast</li>
-          <li>Screenflow</li>
+          <li>I just use native terminal when I'm on Unix.</li>
+          <li>
+            <Link
+              link="https://github.com/PowerShell/PowerShell"
+              text="PowerShell"
+            />
+            &nbsp;when I'm on Windows.
+          </li>
         </ul>
-        <h3 className="mt-4 mb-1" id="music">
-          Music
+        <h3 className="mt-4 mb-1" id="software">
+          Desktop Apps
         </h3>
         <ul className="ml-5 list-disc text-neutral-500">
-          <li>Yamaha P-105 Piano</li>
-          <li>Roland TD-20 V-Drums</li>
-          <li>Roland AC-60</li>
-          <li>Shure SM58</li>
-          <li>Martin Acoustic Guitar (X-Series)</li>
+          <li>
+            I prefer <Link link="https://google.com/chrome" text="Chrome" />
+            &nbsp;Edition for both browsing and development.
+          </li>
+          <li>
+            <Link link="https://www.postman.com/" text="Postman" />
+            &nbsp;for API development and testing.
+          </li>
+          <li>
+            <Link link="https://www.figma.com/" text="Figma" />
+            &nbsp;mainly for inspecting designs.
+          </li>
+          <li>
+            <Link link="https://discord.com/" text="Discord" />
+            &nbsp;and&nbsp;
+            <Link link="https://telegram.org/" text="Telegram" />
+            &nbsp;for messaging or calls.
+          </li>
+          <li>
+            <Link link="https://www.spotify.com/" text="Spotify" /> for music.
+          </li>
+        </ul>
+        <h3 className="mt-4 mb-1" id="other-tech">
+          Desk Setup
+        </h3>
+        <ul className="ml-5 list-disc text-neutral-500">
+          <li>
+            Laptop:{' '}
+            <Link
+              link="https://www.lenovo.com/lt/lt/laptops/lenovo/g-series/g505/"
+              text="Lenovo G505"
+            />
+            &nbsp;15.6" AMD
+          </li>
+          <li>
+            Processor:{' '}
+            <Link
+              link="https://www.amd.com/en/support/apu/amd-series-processors/amd-a4-series-apu-for-desktops/a4-5000-radeon-hd-8330"
+              text="AMD A4"
+            />
+          </li>
+          <li>RAM: 4GB</li>
         </ul>
         <h3 className="mt-4 mb-1" id="other-tech">
           Other Tech
@@ -85,10 +120,7 @@ export default function Uses() {
         <ul className="ml-5 list-disc text-neutral-500">
           <li>Apple Airpods Pro</li>
           <li>Apple Watch</li>
-          <li>Apple iPhone</li>
-          <li>Lacie USB-C External HDD</li>
-          <li>Kindle</li>
-          <li>Tesla Model 3</li>
+          <li>Redmi Note 10 (5G)</li>
         </ul>
       </div>
     </section>
